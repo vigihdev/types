f5802da62202eefca06a9f1d1d8ea2ecdcba17f5
vigih dev <vigihdev@gmail.com>
2025-11-13 13:25:08 +0700

    💥 13-11-2025 11-7

--- index.d.ts
+++ index.d.ts
@@ -3,10 +3,22 @@
 // Definitions by: Vigihdev Developer <your-email@example.com>
 
 import Handlebars from "./handlebars";
+import { TempusDominus } from "./tempus-dominus";
+import { DateTime, DefaultOptions, extend, locale, Namespace, TempusDominus, Unit } from "./tempus-dominus";
 
 declare global {
     var moment: typeof import('./moment');
     var Handlebars: Handlebars;
+    var tempusDominus: {
+        TempusDominus: TempusDominus
+        DateTime: DateTime,
+        DefaultOptions: DefaultOptions,
+        extend: extend,
+        locale: locale,
+        Namespace: Namespace,
+        Unit: Unit
+    }
+
 }
 
 export { }
