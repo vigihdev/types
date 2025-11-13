0ecc67e78965b15c009832bf728cb02782e5eb7a
vigih dev <vigihdev@gmail.com>
2025-11-13 13:09:34 +0700

    💥 13-11-2025 11-7

--- index.d.ts
+++ index.d.ts
@@ -2,9 +2,11 @@
 // Project: https://github.com/vigihdev/types
 // Definitions by: Vigihdev Developer <your-email@example.com>
 
+import Handlebars from "./handlebars";
 
 declare global {
     var moment: typeof import('./moment');
+    var Handlebars: Handlebars;
 }
 
 export { }
